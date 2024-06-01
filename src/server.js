f2566cdf0867e4ea6ae8e09e62856810ce631869
son0925 <shs00925@naver.com>
2024-06-01 17:08:04 +0900

    reStart Word

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server Listenner on ${port}`)
+})
