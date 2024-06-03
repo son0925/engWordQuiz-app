7305e012bb9f0de7990cecad3a368339fe162ee5
son0925 <shs00925@naver.com>
2024-06-03 14:58:54 +0900

    회원가입 비밀번호 암호화

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,21 +1,40 @@
 const userModel = require("../models/users.model");
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 // 회원가입
 async function signupUser(req,res) {
   const { username, password } = req.body;
-  const user = new userModel({username, password})
-
+  
   // db에 해당 유저이름이 있는지
   const findUser = await userModel.findOne({username: username})
   if (findUser) {
     return res.json({msg: '이미 존재하는 유저 이름입니다'})
   }
 
+  const regexPW = /^(?=.*[A-Z])(?=.*[a-z](?=.*\d)(?=.*[!@#$%^&?])[A-Za-z\d!@#$%^&?]{7,})/;
+  if (!regexPW.test(password)) {
+    return res.json({msg: '비밀번호 형식에 맞지 않습니다'})
+  } 
+
+  const hashedPassword = async(password) => {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  }
+  const finallyHashing = await hashedPassword(password)
+  const user = new userModel({
+    username: username,
+    password: finallyHashing
+  })
+  console.log(user)
+
   // db에 유저 정보 저장
   try {
     await user.save()
     return res.json({msg: '회원가입이 완료되었습니다!!'})
   } catch (error) {
+    console.log(error)
     return res.json({msg: '회원가입에 실패했습니다'})
   }
 }
